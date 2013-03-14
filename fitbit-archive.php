5906a63102bcdbd2959ee64df46c1182992f27c0
jeremyfelt <jeremy.felt@gmail.com>
2013-03-13 23:26:20 -0500

    Basic plugin headers, GPL license

--- fitbit-archive.php
+++ fitbit-archive.php
@@ -1,4 +1,29 @@
 <?php
+/*
+Plugin Name: Fitbit Archive
+Plugin URI: http://fitbit.com/jeremyfelt/fitbit-archive/
+Description:
+Version: 0.1
+Author: Jeremy Felt
+Author URI: http://jeremyfelt.com
+License: GPL2
+*/
+
+/*  Copyright 2013 Jeremy Felt (email: jeremy.felt@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 include __DIR__ . '/includes/fitbitphp.php';
 include __DIR__ . '/includes/config.php';
